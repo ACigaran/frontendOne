7a9568e8d41d07d9a9f5eb8733f63a7b9a527024
Mati <mperezfrontan@gmail.com>
2025-09-17 15:01:20 -0300

    mati section updated

--- logica/script.js
+++ logica/script.js
@@ -19,3 +19,26 @@ function scrollCentrado(event, id) {
 
 
 }
+
+
+let idEscribir
+
+function escribirConDelay() {
+  const texto = ("Nose que escribir aca la verdad pero queria meter alguna animacion asi que bueno...\n"+
+    "— ¿Por qué los programadores siempre confunden la Navidad con Halloween?\n"+
+    "— Porque null siempre es un regalo inesperado. 🎁💻\n"
+  );
+  const parrafo = document.getElementById("matias-parrafo");
+  let index = 0;
+  clearTimeout(idEscribir)
+  parrafo.textContent = ""
+  escribir(texto, parrafo, index)
+}
+
+function escribir(texto, parrafo, index){
+    if (index < texto.length) {
+        parrafo.textContent += texto[index];
+        index++;
+        idEscribir = setTimeout(() => escribir(texto, parrafo, index), 65)
+    }
+}
